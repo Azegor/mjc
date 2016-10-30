4ca0c76a1b3c1207c0e9a6adce30af3f99507f75
Timm Bäder <mail@baedert.org>
2016-10-30 09:49:37 +0100

    tests: Add lextest for all keywords and operators

---
+++ test/lextest/all.java
@@ -0,0 +1,19 @@
+abstract, assert, boolean, break, byte, case, catch, char, class, const,
+continue, default, double, do, else, enum, extends, false, finally, final,
+float, for, goto, if, implements, import, instanceof, interface, int, long,
+native, new, null, package, private, protected, public, return, short,
+static, strictfp, super, switch, synchronized, this, throws, throw,
+transient, true, try, void, volatile, while
+
+
+
+!=, !, (, ), *=, *, ++, +=, +, ,, -=, --, -, ., /=,
+/, :, ;, <<=, <<, <=, <, ==, =, >=, >>=, >>>=, >>>, >>, >, ?, %=, %, &=, &&, &, [, ],
+^=, ^, {, }, ~, |=, ||, |
+
+
+some_ident
+another_ident_with_some_numbers123487943767abc
+
+12349578691781765934
+13533
