d49010e9298f0ec612f9230059b247f0ce7ce1c7
Benedikt Morbach <benedikt.morbach@googlemail.com>
2016-11-08 14:28:17 +0100

    add lextests from mjtest

---
+++ test/lextest/comment.java
@@ -0,0 +1,6 @@
+/* this comment
+spans a
+/* whats that? ifnore it!
+few
+lines
+*/
---
+++ test/lextest/keywords.java
@@ -0,0 +1,6 @@
+abstract assert boolean break byte case catch char class const
+continue default double do else enum extends false finally final
+float for goto if implements import instanceof interface int long
+native new null package private protected public return short
+static strictfp super switch synchronized this throws throw
+transient true try void volatile while
