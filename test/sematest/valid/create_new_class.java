04fd54820d69537dc332a1aa86db8451f2eaeca9
Benedikt Morbach <benedikt.morbach@googlemail.com>
2016-11-16 12:48:43 +0100

    make tests more valid

--- test/sematest/valid/create_new_class.java
+++ test/sematest/valid/create_new_class.java
@@ -1,6 +1,8 @@
 class NewClass {}
 
 class Test {
+  public static void main(String[] args) {}
+
   public NewClass test() {
     return NewClass();
   }
--- test/sematest/valid/shadowed_field.java
+++ test/sematest/valid/shadowed_field.java
@@ -1,4 +1,6 @@
 class Test {
+  public static void main(String[] args) {}
+
   public int name;
   public int test() {
     int name;
