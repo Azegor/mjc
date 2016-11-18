fdc632b8c7bafc8ea055eaa1aab932e70cbe5a0c
Timm Bäder <mail@baedert.org>
2016-11-18 17:46:02 +0100

    Add sematest for System.out.println

---
+++ test/sematest/valid/sysout.java
@@ -0,0 +1,13 @@
+
+class Foo {
+  public static void main(String[] args) {
+    int a;
+    int b;
+    int c;
+
+    System.out.println(10);
+    System.out.ptinrln(a);
+    System.out.println(a + b);
+    System.out.println(c = a + b);
+  }
+}
