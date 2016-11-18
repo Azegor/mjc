f68da04bb7c0d08a8f6340f3b6f116bcf31e9f28
Benedikt Morbach <benedikt.morbach@googlemail.com>
2016-11-18 01:04:22 +0100

    ignore output in coverage tests
    afaict it isn't important? Also, the test was always broken and ignored
    the output anyway...
    
    And it runs the --*fuzz commands, so checking the output doesn't even
    make sense

--- test/coverage_invalid.sh
+++ test/coverage_invalid.sh
@@ -8,14 +8,6 @@ compiler_out=$(${compiler} ${arguments} 2>&1)
 compiler_retval=$?
 
 
-# if the return code not 0, "error" must be in the output
-if [[ ${compiler_retval} -ne 0 && ${compiler_out} != *"error"* ]]; then
-  echo "ERROR: Compiler return value is != 0 but output doesn't contain 'error'"
-  echo "Output:"
-  echo "${compiler_out}"
-  exit 1
-fi
-
 if [[ "${compiler_retval}" -eq 0 ]]; then
   echo "ERROR: Compiler returned ${compiler_retval} but '${compiler} ${arguments}' is supposed to be invalid"
   exit 1
