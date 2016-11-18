88b32dec93b8ac39d3695c1ee0d818460bb9061c
Timm Bäder <mail@baedert.org>
2016-11-18 17:24:25 +0100

    Add some more semantic tests

---
+++ test/sematest/invalid/invalid_lvalue.java
@@ -0,0 +1,15 @@
+class Foo {
+  public int f() { return 3; }
+
+  public static void main(String[] args) {
+    true = true;
+
+    1 = 3 + 2;
+
+    null = new Foo();
+
+    Foo f;
+
+    f.f() = 10;
+  }
+}
