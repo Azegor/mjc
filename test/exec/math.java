22dadd65cea23d6bbfa28b0043c444a1f404e23f
Benedikt Morbach <benedikt.morbach@googlemail.com>
2016-11-20 02:41:24 +0100

    fix broken mjtest tests

--- test/exec/math.java
+++ test/exec/math.java
@@ -1,45 +1,6 @@
 import lib.Math;
 import lib.LehmerRandom;
-
-class ArrayUtils {
-  public ArrayUtils printIntArray(int[] arr, int size){
-    int i = 0;
-    while (i < size){
-      System.out.println(arr[i]);
-      i = i + 1;
-    }
-    return this;
-  }
-
-  public ArrayUtils printBooleanArray(boolean[] arr, int size){
-    int i = 0;
-    while (i < size){
-      System.out.println(arr[i]);
-      i = i + 1;
-    }
-    return this;
-  }
-
-  public int[] copyIntArray(int[] src, int start, int length){
-    int[] ret = new int[length];
-    int i = start;
-    while (i < start + length){
-      ret[i - start] = src[i];
-      i = i + 1;
-    }
-    return ret;
-  }
-
-  public int toInt(int[] chars, int size){
-    int val = 0;
-    size = size - 1;
-    while (size >= 0){
-      val = val * 10 + chars[size] - new ASCII().init()._0;
-      size = size - 1;
-    }
-    return val;
-  }
-}
+import lib.ArrayUtils;
 
 class Terminals {
   public Terminal number;
--- test/testlib/lib/ArrayUtils.java
+++ test/testlib/lib/ArrayUtils.java
@@ -10,15 +10,6 @@ public class ArrayUtils {
     return this;
   }
 
-  public ArrayUtils printBooleanArray(boolean[] arr, int size){
-    int i = 0;
-    while (i < size){
-      System.out.println(arr[i]);
-      i = i + 1;
-    }
-    return this;
-  }
-
   public int[] copyIntArray(int[] src, int start, int length){
     int[] ret = new int[length];
     int i = start;
@@ -29,4 +20,14 @@ public class ArrayUtils {
     return ret;
   }
 
+  public int toInt(int[] chars, int size){
+    int val = 0;
+    size = size - 1;
+    while (size >= 0){
+      val = val * 10 + chars[size] - new ASCII().init()._0;
+      size = size - 1;
+    }
+    return val;
+  }
 }
+
