82b820bab4a37c86072dbd89781e602b9dbaa71e
Tilman <azegor@hotmail.de>
2016-11-30 01:15:25 +0100

    more tests

---
+++ test/firmtest/array_field_access.java
@@ -0,0 +1,14 @@
+class Foobar {
+  public int[] a;
+
+  public int[] foo() {
+    return this.a;
+  }
+  public int[] bar() {
+    return a;
+  }
+
+
+  public static void main(String[] args) {
+  }
+}
