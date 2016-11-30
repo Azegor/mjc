58fcf7965c830ba70c22ef48efdf7a64ff83bd2b
Tilman <azegor@hotmail.de>
2016-12-01 00:22:14 +0100

    fix boolean_expression_chain.java to have only one return statement

--- test/firmtest/boolean_expression_chain.java
+++ test/firmtest/boolean_expression_chain.java
@@ -3,7 +3,12 @@ class Foo {
   }
   public boolean foo(int a, int b) {
       return (5 == 5) == false; 
+  }
+  public boolean bar(int a, int b) {
       return (a == b);
+  }
+  public boolean baz(int a, int b) {
 /*    boolean b = true && false || true; */
+    return false;
   }
 }
