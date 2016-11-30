fe1708ce96871a45d6106e8e3ef7ef4d5d032516
Benedikt Morbach <benedikt.morbach@googlemail.com>
2016-11-30 16:03:19 +0100

    add test for expression chaining

---
+++ test/firmtest/boolean_expression_chain.java
@@ -0,0 +1,9 @@
+class Foo {
+  public static void main(String[] args) {
+  }
+  public boolean foo(int a, int b) {
+      return (5 == 5) == false; 
+      return (a == b);
+/*    boolean b = true && false || true; */
+  }
+}
