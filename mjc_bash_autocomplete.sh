b85c4926599eb72153e10282afa665d0c28eece3
Tilman <azegor@hotmail.de>
2016-12-12 19:13:21 +0100

    added bash autocomplete file - 'source' to use it

---
+++ mjc_bash_autocomplete.sh
@@ -0,0 +1,15 @@
+# _mjc()
+# {
+#   _mjc_commands=$(./mjc --help)
+#
+#   local cur prev
+#   COMPREPLY=()
+#   cur="${COMP_WORDS[COMP_CWORD]}"
+#   COMPREPLY=( $(compgen -W "${_script_commands}" -- ${cur}) )
+#
+#   return 0
+# }
+# complete -o nospace -F _mjc ./mjc
+
+complete_words='-h --help --echo --input-file --lextest --lexfuzz --parsetest --parsefuzz --print-ast --dot-ast --check --fuzz-check --dot-attr-ast --firm-graph --compile-firm -S --output-assembly --no-verify -O --optimize -o'
+complete -o nospace -W "${complete_words}" -o bashdefault -o default ./mjc
