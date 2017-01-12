e7dbbeba1a30f3bcbdb7ec5c3601351f89b81c15
Timm Bäder <mail@baedert.org>
2017-01-12 19:08:40 +0100

    tests: Add ASM call test

---
+++ test/asm/call.java
@@ -0,0 +1,14 @@
+class Foobar {
+
+  public void zomg() {
+    System.out.println(60);
+  }
+
+
+  public static void main(String[] args) {
+    Foobar f = new Foobar();
+    f.zomg();
+    f.zomg();
+    f.zomg();
+  }
+}
