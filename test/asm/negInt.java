45b9e0054c974cc7f8f3ffc402ab2b16816f0fbe
Timm Bäder <mail@baedert.org>
2017-01-12 19:43:40 +0100

    tests: Add ASM test for negative integers

---
+++ test/asm/negInt.java
@@ -0,0 +1,14 @@
+class Foobar {
+  public static void main(String[] args) {
+    int a = 1 + (-2);
+
+    int b = 1 - 2;
+
+    if (a == b) {
+      System.out.println(1);
+    } else {
+      System.out.println(a);
+      System.out.println(b);
+    }
+  }
+}
