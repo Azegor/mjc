efd69af8adc0a366ae779b228158a9b332054342
Timm Bäder <mail@baedert.org>
2017-01-12 18:56:31 +0100

    tests: Add another ASM array access test

---
+++ test/asm/arrayAccess2.java
@@ -0,0 +1,25 @@
+class Foobar {
+  public static void main(String[] args) {
+    int[] ints = new int[10];
+    ints[0] = 0;
+    ints[1] = 1;
+    ints[2] = 2;
+    ints[3] = 3;
+    ints[4] = 4;
+    ints[5] = 5;
+    ints[6] = 6;
+    ints[7] = 7;
+    ints[8] = 8;
+    ints[9] = 9;
+    System.out.println(ints[0]);
+    System.out.println(ints[1]);
+    System.out.println(ints[2]);
+    System.out.println(ints[3]);
+    System.out.println(ints[4]);
+    System.out.println(ints[5]);
+    System.out.println(ints[6]);
+    System.out.println(ints[7]);
+    System.out.println(ints[8]);
+    System.out.println(ints[9]);
+  }
+}
