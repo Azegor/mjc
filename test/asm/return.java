dd13d7d76cd8dab5a05e6acb0ea65c92dd72b9a6
Timm Bäder <mail@baedert.org>
2017-01-12 19:21:54 +0100

    tests: Add return value ASM test

---
+++ test/asm/return.java
@@ -0,0 +1,11 @@
+class Foobar {
+
+  public int zomg() {
+    return 1337;
+  }
+
+  public static void main(String[] args) {
+    Foobar f = new Foobar();
+    System.out.println(f.zomg());
+  }
+}
