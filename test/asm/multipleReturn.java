a8cadbb602c3b9f7038a10dbf79a65972b6a5ded
Timm Bäder <mail@baedert.org>
2017-01-12 12:37:33 +0100

    tests: add asm multiple return test
    And add the missing .out files (ignored by .gitignore :V )

---
+++ test/asm/multipleReturn.java
@@ -0,0 +1,16 @@
+class Foobar {
+  public static void main(String[] args) {
+    int k = System.in.read();
+
+    if (k == 97) {
+      System.out.println(1);
+      return;
+    } else {
+      System.out.println(0);
+      return;
+    }
+
+    System.out.println(1337);
+  }
+}
+
