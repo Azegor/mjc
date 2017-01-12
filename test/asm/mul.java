fe960f9e9f84898d39b76f8d14b72abd6723f243
Timm Bäder <mail@baedert.org>
2017-01-12 17:07:32 +0100

    tests: Add mul and mul+add ASM tests

---
+++ test/asm/mul.java
@@ -0,0 +1,6 @@
+class Foobar {
+  public static void main(String[] args) throws Foobar {
+    int k = 13 * 2;
+    System.out.println(k);
+  }
+}
---
+++ test/asm/mulAdd.java
@@ -0,0 +1,7 @@
+class Foobar {
+  public static void main(String[] args) throws Foobar {
+    int k = 10 * 2 + 3;
+
+    System.out.println(k);
+  }
+}
