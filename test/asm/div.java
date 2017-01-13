7cb1ba28b7cf953abc68dae36bb8e24d00c5f125
Timm Bäder <mail@baedert.org>
2017-01-13 10:37:17 +0100

    tests: Add ASM div test

---
+++ test/asm/div.java
@@ -0,0 +1,8 @@
+class Foobar {
+  public static void main(String[] args) {
+    System.out.println(10 / 4);
+    System.out.println(10 / 2);
+    System.out.println(10 / 1);
+    System.out.println(1 / 10);
+  }
+}
