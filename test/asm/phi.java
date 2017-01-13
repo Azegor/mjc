67f0db7b11bae8df60a655feea383938eccf8a0c
Timm Bäder <mail@baedert.org>
2017-01-13 03:41:26 +0100

    tests: Add ASM test for phi nodes

---
+++ test/asm/phi.java
@@ -0,0 +1,13 @@
+class Foobar {
+  public static void main(String[] args) {
+    int k = 1;
+    int a;
+    if (k == 2) {
+      a = 3;
+    } else {
+      a = 4;
+    }
+
+    System.out.println(a);
+  }
+}
