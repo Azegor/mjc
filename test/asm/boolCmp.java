533501d5531548a340225c8b5d0e7498fc1fd4cf
Timm Bäder <mail@baedert.org>
2017-01-14 16:04:47 +0100

    tests: Add ASM boolCmp test

---
+++ test/asm/boolCmp.java
@@ -0,0 +1,12 @@
+class Foobar {
+  public static void main(String[] args) {
+    boolean b = (System.in.read() == 97);
+    if (b) {
+      System.out.println(1);
+    }else {
+      System.out.println(0);
+    }
+  }
+}
+
+
