4cabc89152c409ccadf784736e6fd2fccdb0240d
Timm Bäder <mail@baedert.org>
2017-01-15 14:37:34 +0100

    Asm::Function: Don't write add/sub instructions for stack size 0

--- src/asm.cpp
+++ src/asm.cpp
@@ -119,13 +119,15 @@ void Function::writeProlog(AsmWriter &writer) const {
   // TODO do the "right way" with writeInstruction!
   writer.writeString("pushq %rbp");
   writer.writeString("mov %rsp, %rbp");
-  writer.writeString("subq $" + std::to_string(ARsize) + ", %rsp");
+  if (ARsize > 0)
+    writer.writeString("subq $" + std::to_string(ARsize) + ", %rsp");
   writer.writeLabel('.' + fnName.name + "_body");
   writer.writeString("jmp .L" + std::to_string(startBlockId));
 }
 void Function::writeEpilog(AsmWriter &writer) const {
   writer.writeLabel('.' + this->getEpilogLabel());
-  writer.writeString("addq $" + std::to_string(ARsize) + ", %rsp");
+  if (ARsize > 0)
+    writer.writeString("addq $" + std::to_string(ARsize) + ", %rsp");
   writer.writeString("leave");
   writer.writeString("ret");
 }
